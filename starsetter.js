9f040d830cbea7df0a97cbec2d6714ef61e4616d
Cheese Cake Aficionado <sasha@fastmail.com>
2018-09-21 18:33:22 -0700

    Update starsetter.js

--- starsetter.js
+++ starsetter.js
@@ -1,5 +1,5 @@
 (function() {
-  let b = document.querySelector(".social-count.js-social-count"); 
+  let b = document.querySelector('.social-count.js-social-count'); 
   let stars = parseInt(b.textContent);
   let newStars = stars += Math.ceil(stars / 20);
   b.innerHTML = newStars;
